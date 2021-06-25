c2de82521c2366d33193e324af833e263afa2264
kuro-siba <siba-kuro@nike.eonet.ne.jp>
2021-06-25 23:19:50 +0900

    Update memo data on MemoEditScreen

--- src/screens/MemoDetailScreen.jsx
+++ src/screens/MemoDetailScreen.jsx
@@ -47,14 +47,14 @@ export default function MemoDetailScreen(props) {
       <CircleButton
         style={{ top: 60, bottom: 'auto' }}
         name="pencil"
-        onPress={() => { navigation.navigate('MemoEdit'); }}
+        onPress={() => { navigation.navigate('MemoEdit', { id: memo.id, bodyText: memo.bodyText }); }}
       />
     </View>
   );
 }
 
 MemoDetailScreen.propTypes = {
-  routes: shape({
+  route: shape({
     params: shape({ id: string }),
   }).isRequired,
 };
